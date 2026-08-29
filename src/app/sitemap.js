8a6102e43fced2e15eb874e90560dc1db639a8b9
dls19004 <dls19004@gmail.com>
2026-08-28 19:05:20 -0500

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://vortextrailstudio.com", lastModified: new Date() },
+    { url: "https://vortextrailstudio.com/games", lastModified: new Date() },
+    { url: "https://vortextrailstudio.com/studio", lastModified: new Date() },
+    { url: "https://vortextrailstudio.com/news", lastModified: new Date() },
+  ];
+}
